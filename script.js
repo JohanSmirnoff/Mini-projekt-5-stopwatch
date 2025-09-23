d79032416d450e1cf2b849631234152ec197f9bc
JohanSmirnoff <smirnoff.johan@gmail.com>
2025-09-23 10:32:52 +0200

    färdig sida

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let startTime = 0;
+let elapsedTime = 0;
 let intervalID = null;
 let running = false;
 
@@ -8,15 +9,26 @@ const stopButton = document.getElementById("stop-btn")
 const resetButton = document.getElementById("reset-btn")
 let timertime = document.getElementById("timertime")
 
-function ms() {
-    document.getElementById("timertime").innerHTML = startTime
-    startTime++
+    function formatera(tid) {
+        const totalTid = Math.floor(tid / 1000)
+        const minuter = Math.floor(totalTid / 60)
+        const sekunder = totalTid % 60
+        const hundraDelar = Math.floor((tid % 1000) / 10)
+        return `${String(minuter).padStart(2, "0")}:${
+            String(sekunder).padStart(2, "0")}:${String(hundraDelar).padStart(2, "0")}`
+        }
+        
+        function uppdatera() {
+            const nuvarandeTime = Date.now()
+            const runningTime = nuvarandeTime - startTime
+            timertime.textContent = formatera(runningTime)
+            
 }
-    
     
     startButton.addEventListener("click", () => {
         if (!running) {
-            intervalID = setInterval(ms, 10)
+            startTime = Date.now() - elapsedTime
+            intervalID = setInterval(uppdatera, 10)
             running = true
         }
 })
@@ -24,9 +36,19 @@ function ms() {
     stopButton.addEventListener("click", () => {
         if (running) {
             clearInterval(intervalID)
+            elapsedTime = Date.now() - startTime
             running = false
         }
     })
+    resetButton.addEventListener("click", () => {
+            clearInterval(intervalID)
+            running = false
+            startTime = "00:00:00"
+            timertime.innerHTML = startTime
+        }
+    )
+ 
+
 
 
 
