942779a26987ddd59ed442f51524df825d772adb
JohanSmirnoff <smirnoff.johan@gmail.com>
2025-09-23 09:05:17 +0200

    test av formatering

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 let startTime = 0;
+let runningTime = 0;
 let intervalID = null;
-let running = false;
 
+let running = false;
 
 const startButton = document.getElementById("start-btn")
 const stopButton = document.getElementById("stop-btn")
 const resetButton = document.getElementById("reset-btn")
 let timertime = document.getElementById("timertime")
 
+function formatera(tid) {
+    const totalTid = Math.floor(tid / 1000)
+    const minuter = Math.floor(totalTid / 60)
+    const sekunder = totalTid & 60
+    const hundraDelar = Math.floor((totalTid & 1000) / 10)
+    return `${String(minuter).padStart(2, "0")}:${
+        String(sekunder).padStart(2, "0")}:${String(hundraDelar).padStart(2, "0")}`
+}
+
+function uppdatera() {
+
+}
+
+
+
+
+
+
+
+
+
+
 function ms() {
     document.getElementById("timertime").innerHTML = startTime
     startTime++
 }
+
 
 // function stopTime() {
     //     clearInterval(interval)
@@ -37,16 +61,6 @@ function ms() {
         }
     })
 
-
-
-
-
-
-
-
-
-
-    
     // function stopTime() {
     // if (running === true) {
     //     clearInterval()
